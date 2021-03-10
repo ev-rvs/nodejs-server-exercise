18488cbce4eafefade40c71c1f928657cdb10680
RVS <rob@elephantventures.com>
2021-03-10 15:54:38 -0500

    feat(evobe-87, evobe-83): added ping counts for pong endpoint

--- node/routes/pong.js
+++ node/routes/pong.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.status('200').json({ message: "PONG" });
+const { getAllPongEventCount } = require('../db/sequelize');
+
+router.get('/', async (req, res, next) => {
+  const pongCount = await getAllPongEventCount();
+  if (pongCount && typeof pongCount.count !== 'undefined') {
+    res.status('200').json({ message: "PING", pingCount: pongCount.count });
+  } else {
+    res.status('400').json({ message: "There was an error with your pong count." });
+  }
 });
 
 module.exports = router;
