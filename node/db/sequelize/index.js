7a75448db8a4b0d40b858a4be5de9827b83c5ceb
RVS <rob@elephantventures.com>
2022-03-21 13:09:04 -0400

    chore: allowed skip for integration testing, no localized db atm

--- node/db/sequelize/index.js
+++ node/db/sequelize/index.js
@@ -8,6 +8,7 @@ const dbHost = (process.env.NODE_ENV === 'test') ? process.env.TEST_DATABASE_HOS
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
   host: dbHost,
   dialect: 'postgres',
+  port: process.env.POSTGRES_PORT,
 });
 
 // initialize models
